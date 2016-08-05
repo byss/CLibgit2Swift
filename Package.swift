51c38a53be75ece31178cff44dc754e78b9b9aca
Benjamin Müller <elchbenny@googlemail.com>
2016-08-05 16:26:19 +0200

    new way of import

--- Package.swift
+++ Package.swift
@@ -1,5 +1,10 @@
 import PackageDescription
 
 let package = Package(
-    name: "CLibgit2Swift"
+    name: "CLibgit2Swift",
+    pkgConfig: "libgit2",
+    providers: [
+        .Brew("libgit2"),
+        .Apt("libgit2-dev")
+    ]
 )
--- libgit2.h
+++ libgit2.h
@@ -1,2 +1,61 @@
-#import "include/git2.h"
+/*
+ * Copyright (C) the libgit2 contributors. All rights reserved.
+ *
+ * This file is part of libgit2, distributed under the GNU GPL v2 with
+ * a Linking Exception. For full terms see the included COPYING file.
+ */
 
+
+#import <git2/annotated_commit.h>
+#import <git2/attr.h>
+#import <git2/blob.h>
+#import <git2/blame.h>
+#import <git2/branch.h>
+#import <git2/buffer.h>
+#import <git2/checkout.h>
+#import <git2/cherrypick.h>
+#import <git2/clone.h>
+#import <git2/commit.h>
+#import <git2/common.h>
+#import <git2/config.h>
+#import <git2/describe.h>
+#import <git2/diff.h>
+#import <git2/errors.h>
+#import <git2/filter.h>
+#import <git2/global.h>
+#import <git2/graph.h>
+#import <git2/ignore.h>
+#import <git2/index.h>
+#import <git2/indexer.h>
+#import <git2/merge.h>
+#import <git2/message.h>
+#import <git2/net.h>
+#import <git2/notes.h>
+#import <git2/object.h>
+#import <git2/odb.h>
+#import <git2/odb_backend.h>
+#import <git2/oid.h>
+#import <git2/pack.h>
+#import <git2/patch.h>
+#import <git2/pathspec.h>
+#import <git2/rebase.h>
+#import <git2/refdb.h>
+#import <git2/reflog.h>
+#import <git2/refs.h>
+#import <git2/refspec.h>
+#import <git2/remote.h>
+#import <git2/repository.h>
+#import <git2/reset.h>
+#import <git2/revert.h>
+#import <git2/revparse.h>
+#import <git2/revwalk.h>
+#import <git2/signature.h>
+#import <git2/stash.h>
+#import <git2/status.h>
+#import <git2/submodule.h>
+#import <git2/tag.h>
+#import <git2/transport.h>
+#import <git2/transaction.h>
+#import <git2/tree.h>
+#import <git2/types.h>
+#import <git2/version.h>
