95eac7e8e07efe371f8c49b8303a7eb112d3912c
Benjamin Müller <elchbenny@googlemail.com>
2016-08-05 15:48:06 +0200

    changed package config

--- Package.swift
+++ Package.swift
@@ -1,10 +1,5 @@
 import PackageDescription
 
 let package = Package(
-    name: "CLibgit2Swift",
-    pkgConfig: "libgit2",
-    providers: [
-		   .Brew("libgit2"),
-		   .Apt("libgit2-dev")
-	  ]
+    name: "CLibgit2Swift"
 )
