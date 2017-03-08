f08b43cd651a15ea14a6f20dd6dbf8109fa160ea
Kirill byss Bystrov <kirrbyss@gmail.com>
2017-03-08 11:56:06 +0300

    Fixed package code formatting

--- Package.swift
+++ Package.swift
@@ -1,10 +1,11 @@
 import PackageDescription
 
-let package = Package(
+let package = Package (
     name: "Clibgit2",
+
     pkgConfig: "libgit2",
     providers: [
-        .Brew("libgit2"),
-        .Apt("libgit2-dev")
+        .Brew ("libgit2"),
+        .Apt ("libgit2-dev"),
     ]
-)
+);
